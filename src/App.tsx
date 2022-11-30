5e99d0298659c4b501e0b7d8952daf66df65f62e
Jeimissonb <jeimissonb@hotmail.com>
2022-11-29 23:21:52 -0300

    changing project to tsx

---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import { Header } from "./Components/Header";
+import { Sidebar } from "./Components/Sidebar";
+import { Post } from "./Components/Post";
+
+import "./global.css";
+
+import styles from "./App.module.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/jeimissonb.png",
+      name: "Jeimisson Barbosa",
+      role: "Desenvolvedor Fullstack",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz",
+      },
+      {
+        type: "paragraph",
+        content:
+          "no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-11-20 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz",
+      },
+      {
+        type: "paragraph",
+        content:
+          "no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-11-27 20:00:00"),
+  },
+];
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
